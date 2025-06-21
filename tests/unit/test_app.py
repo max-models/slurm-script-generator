d445de6a8b01c348ef5440ec6229b4f63f9f4e1f
Max <max.lindqvist@ipp.mpg.de>
2025-06-21 12:54:27 +0200

    Update main2 (#10)
    * Added --vars (#1)
    
    * Setup python module (#3)
    
    * Renamed
    
    * return script
    
    * added test_options
    
    * Corrected script path
    
    * Replaced _ with - for slurm commands (#6)
    
    * Replaced _ with - for slurm commands
    
    * Fixed test
    
    * Add important features (#8)
    
    * Added Pragma class
    
    * Added parser for slurm groff page
    
    * Added method to the Pragma class
    
    * Removed src/slurm_script_generator/parse_groff.py
    
    * Added the arguments with the Pragma class
    
    * Formatting
    
    * Added generate_script
    
    * Bugfixes
    
    * fixes
    
    * Check that the output is correct in the CLI
    
    * 2 add unit tests for all cli options (#9)
    
    * Extended tests a bit
    
    * Added examples to some of the Pragmas, all these examples are tested
    
    * isort
    
    * restored the files from devel

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,3 +1,9 @@
+import pytest
+
+from slurm_script_generator.main import generate_script
+from slurm_script_generator.sbatch_parser import pragmas
+
+
 def test_import_app():
     from slurm_script_generator.main import main
 
@@ -5,13 +11,66 @@ def test_import_app():
 
 
 def test_options():
-    from slurm_script_generator.main import generate_script
 
-    args = {"nodes": 1, "ntasks-per-node": 16}
-    script = generate_script(args_dict=args)
+    args = {"nodes": 1, "ntasks_per_node": 16}
+    script = generate_script(args_dict=args, print_script=False)
+    print(script)
     assert "#SBATCH --nodes 1" in script
     assert "#SBATCH --ntasks-per-node 16" in script
+
+
+@pytest.mark.parametrize("job_name", ["test_job1", "test_job2"])
+@pytest.mark.parametrize("tasks_per_node", [16, 32, 48])
+@pytest.mark.parametrize("nodes", [1, 2, 3, 4])
+@pytest.mark.parametrize(
+    "modules",
+    [
+        [
+            "gcc/12",
+            "openmpi/4.1",
+            "anaconda/3/2023.03",
+            "git/2.43",
+            "pandoc/3.1",
+            "likwid/5.2",
+        ]
+    ],
+)
+@pytest.mark.parametrize("venv_path", ["~/virtual_envs/env_slurm"])
+@pytest.mark.parametrize("mem", ["25GB"])
+@pytest.mark.parametrize("time", ["00:45:00"])
+def test_default_script(job_name, tasks_per_node, nodes, modules, venv_path, mem, time):
+    """Tests a typical script format."""
+    script_params = {
+        "job_name": job_name,
+        "ntasks_per_node": tasks_per_node,
+        "nodes": nodes,
+        "modules": modules,
+        "likwid": True,
+        "venv": venv_path,
+        "mem": mem,
+        "time": time,
+    }
+    script = generate_script(args_dict=script_params, print_script=False)
+
+    assert f"#SBATCH --job-name {job_name}" in script
+    assert f"#SBATCH --ntasks-per-node {tasks_per_node}" in script
+    assert f"#SBATCH --nodes {nodes}" in script
+    assert f"module load {' '.join(modules)}" in script
+
+
+def test_examples():
+    script_params = {}
+    for p in pragmas:
+        if p.example is not None:
+            script_params[p.dest] = p.example
+
+    script = generate_script(args_dict=script_params, print_script=False)
+
     print(script)
+
+    for p in pragmas:
+        if p.example is not None:
+            assert f"#SBATCH --{p.sbatch_flag} {p.example}" in script
 
 
 if __name__ == "__main__":
