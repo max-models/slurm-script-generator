ffecb57edb36dc644d0ef7dd02dd00fc547f7ef8
Max <max.lindqvist@ipp.mpg.de>
2026-02-21 12:38:09 +0100

    42 add a header (#43)
    * Added a header
    
    * Added a include_header argument
    
    * Added a --no-header flag
    
    * Use to_string in __str__
    
    * Updated README with --no-header

--- src/slurm_script_generator/slurm_script.py
+++ src/slurm_script_generator/slurm_script.py
@@ -1,5 +1,6 @@
 import json
 import os
+from importlib.metadata import version
 from typing import Any, List
 
 from slurm_script_generator.pragmas import Pragma, PragmaFactory
@@ -253,26 +254,43 @@ class SlurmScript:
         else:
             raise ValueError(f"Unknown parameter key: {key}")
 
-    def generate_script(self, line_length: int = 40) -> str:
-        script_repr = "#!/bin/bash\n"
-        script_repr += "#" * (line_length + 2) + "\n"
+    def generate_script(
+        self, line_length: int = 40, include_header: bool = False
+    ) -> str:
+
+        script_str = "#!/bin/bash\n"
+
+        # Add header
+        SLURM_SCRIPT_HEADER = f"""########################################################
+#            This script was generated using           #
+#             slurm-script-generator v{version('slurm-script-generator')}            #
+# https://github.com/max-models/slurm-script-generator #
+#      `pip install slurm-script-generator=={version('slurm-script-generator')}`     #
+########################################################\n
+"""
+        if include_header:
+            script_str += SLURM_SCRIPT_HEADER
+
+        # Add sbatch pragmas
+        line_separator = "#" * (line_length + 2) + "\n"
+        script_str += add_line(line_separator)
         for pragma in self.pragmas:
-            script_repr += f"{pragma}"
-        script_repr += "#" * (line_length + 2) + "\n"
+            script_str += f"{pragma}"
+        script_str += add_line(line_separator)
 
         # Load modules
         if len(self.modules) > 0:
-            script_repr += add_line(
+            script_str += add_line(
                 "module purge",
                 "Purge modules",
                 line_length=line_length,
             )
-            script_repr += add_line(
+            script_str += add_line(
                 f"module load {' '.join(self.modules)}",
                 "modules",
                 line_length=line_length,
             )
-            script_repr += add_line(
+            script_str += add_line(
                 "module list",
                 "List loaded modules",
                 line_length=line_length,
@@ -280,12 +298,12 @@ class SlurmScript:
 
         if len(self.custom_commands) > 0:
             for custom_command in self.custom_commands:
-                script_repr += add_line(
+                script_str += add_line(
                     f"{custom_command}\n",
                     line_length=line_length,
                 )
 
-        return script_repr
+        return script_str
 
     def to_dict(self) -> dict[str, Any]:
         return {
@@ -328,8 +346,11 @@ class SlurmScript:
             return False
         return self.to_dict() == value.to_dict()
 
+    def to_string(self, include_header: bool = True) -> str:
+        return self.generate_script(include_header=include_header)
+
     def __str__(self) -> str:
-        return self.generate_script()
+        return self.to_string(include_header=True)
 
     @property
     def line_length(self) -> int:
