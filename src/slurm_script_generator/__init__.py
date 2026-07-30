7d7c5f86796691c25a80c66b039781ae44410f33
Max <max.lindqvist@ipp.mpg.de>
2026-07-30 11:08:42 +0200

    Add job state (#82)
    * Added a job_state method
    
    * formattign

--- src/slurm_script_generator/__init__.py
+++ src/slurm_script_generator/__init__.py
@@ -1,7 +1,13 @@
 """Public package exports for :mod:`slurm_script_generator`."""
 
 from slurm_script_generator.slurm_script import SlurmScript
-from slurm_script_generator.squeue import SAcct, SAcctJob, SQueue, SQueueJob
+from slurm_script_generator.squeue import (
+    SAcct,
+    SAcctJob,
+    SQueue,
+    SQueueJob,
+    job_state,
+)
 
 __all__ = [
     "SlurmScript",
@@ -9,4 +15,5 @@ __all__ = [
     "SQueueJob",
     "SAcct",
     "SAcctJob",
+    "job_state",
 ]
